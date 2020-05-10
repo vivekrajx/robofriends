55dd8369888f1e4908a01bc7feed11b284d27f64
Vivek Raj <vivekrajx@users.noreply.github.com>
2020-05-10 15:09:06 +0530

    Updates

---
+++ precache-manifest.6ca53d6a3c8a6e330bdcc32622b22469.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9eb9f904a463e4d921a28097731a13",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "77a8f0b803f88f27b559",
+    "url": "/robofriends/static/css/main.ca934655.chunk.css"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a8f0b803f88f27b559",
+    "url": "/robofriends/static/js/main.96ce72c0.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  },
+  {
+    "revision": "86c545ccae4a76a95a9ffdd312221e69",
+    "url": "/robofriends/static/media/MR ROBOT.86c545cc.woff"
+  }
+]);
